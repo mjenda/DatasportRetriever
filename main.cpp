4da7e7544bf29d85709f8ded04a4dbf46a27246f
Jenda Marek <marek.jenda@nokia.com>
2017-06-23 22:31:12 +0200

    Moved table to mainwindow.

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include "DatasportModel.h"
+#include "mainwindow.h"
 
 #include <QApplication>
 #include <QTableView>
@@ -8,18 +9,8 @@ int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
 
-    QTableView tableView {};
-    DatasportModel model {};
-
-    QHeaderView *verticalHeader = tableView.verticalHeader();
-    verticalHeader->setSectionResizeMode(QHeaderView::ResizeMode::Fixed);
-    verticalHeader->setDefaultSectionSize(20);
-
-    tableView.verticalHeader ()->hide();
-    tableView.setModel(&model);
-    tableView.showMaximized();
-
-    model.setCompetition(2020);
+    MainWindow mainWindow;
+    mainWindow.showMaximized();
 
     app.exec();
 }
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,27 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include "DatasportModel.h"
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+
+    DatasportModel * model = new DatasportModel(this);
+
+    QHeaderView *verticalHeader = ui->tableView->verticalHeader();
+    verticalHeader->setSectionResizeMode(QHeaderView::ResizeMode::Fixed);
+    verticalHeader->setDefaultSectionSize(20);
+
+    ui->tableView->verticalHeader ()->hide();
+    ui->tableView->setModel(model);
+    ui->tableView->showMaximized();
+
+    model->setCompetition(2020);
+
 }
 
 MainWindow::~MainWindow()
